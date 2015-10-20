3fc72a3486ee00d10cc38593d0adf2417089c0fa
Mathis Klooß <mathis@mage-profis.de>
2015-10-20 15:37:03 +0200

    add breadcrumb

--- src/app/code/community/MageProfis/RichSnippets/Block/Product.php
+++ src/app/code/community/MageProfis/RichSnippets/Block/Product.php
@@ -4,6 +4,8 @@ class MageProfis_RichSnippets_Block_Product
 extends Mage_Catalog_Block_Product_Abstract
 {
 
+    protected $_breadcrumb = null;
+
     /**
      * encode json with extra options
      * 
@@ -26,6 +28,16 @@ extends Mage_Catalog_Block_Product_Abstract
     }
 
     /**
+     * 
+     * @return string
+     */
+    public function getShortDescription()
+    {
+        $desc = strip_tags($this->getProduct()->getShortDescription());
+        return $this->jsonEncode($desc);
+    }
+
+    /**
      * get Final Price with Tax Calculation
      * 
      * @return string
@@ -35,5 +47,21 @@ extends Mage_Catalog_Block_Product_Abstract
         $price = Mage::helper('tax')
             ->getPrice($this->getProduct(), $this->getProduct()->getFinalPrice(), true);
         return number_format($price, 2, '.', '');
+    }
+
+    /**
+     * get Breadcrumb Path without Product
+     * 
+     * @return array
+     */
+    public function getBreadcrumbPath()
+    {
+        if (is_null($this->_breadcrumb))
+        {
+            $breadcrumb = Mage::helper('catalog')->getBreadcrumbPath();
+            array_pop($breadcrumb); // remove last item
+            $this->_breadcrumb = $breadcrumb;
+        }
+        return $this->_breadcrumb;
     }
 }
