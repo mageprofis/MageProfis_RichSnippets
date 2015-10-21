41da1a6f2574e98128796300c44bc321e4fe5db9
Remi Ebeling <remi.ebeling@mage-profis.de>
2015-10-21 14:44:40 +0200

    fixed modman & nesting errors in config.xml

--- src/app/code/community/MageProfis/RichSnippets/Block/Breadcrumb.php
+++ src/app/code/community/MageProfis/RichSnippets/Block/Breadcrumb.php
@@ -4,6 +4,17 @@ class MageProfis_RichSnippets_Block_Breadcrumb
 extends Mage_Core_Block_Template
 {
     /**
+     * encode json with extra options
+     * 
+     * @param mixed $string
+     * @return string
+     */
+    public function jsonEncode($string)
+    {
+        return Mage::helper('richsnippets')->jsonEncode($string);
+    }
+
+    /**
      * get Breadcrumb Path without Product
      * 
      * @return array
@@ -13,7 +24,7 @@ extends Mage_Core_Block_Template
         if (is_null($this->_breadcrumb))
         {
             $breadcrumb = Mage::helper('catalog')->getBreadcrumbPath();
-            array_pop($breadcrumb); // remove last item
+            //array_pop($breadcrumb); // remove last item
             $this->_breadcrumb = $breadcrumb;
         }
         return $this->_breadcrumb;
