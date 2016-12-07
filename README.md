93ffbcfe309e7b892456f9ca0c175d7eeed1c894
Leon <leon@mage-profis.de>
2016-12-07 10:40:16 +0100

    add product ratings

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
 # MageProfis_RichSnippets
 Add rich snippets to the shop.
 
-Product page (catalog_category_view):   
+Product page (catalog_product_view):   
 - Product data
 - Breadcrump
+- Rating
 
 Category page (catalog_category_view):   
 - Breadcrump
-
-# Todo
-- Rating
--- src/app/code/community/MageProfis/RichSnippets/Block/Product.php
+++ src/app/code/community/MageProfis/RichSnippets/Block/Product.php
@@ -3,6 +3,8 @@
 class MageProfis_RichSnippets_Block_Product
 extends Mage_Catalog_Block_Product_Abstract
 {
+    public $_rating_value = -1;
+    public $_rating_count = -1;
 
     protected $_breadcrumb = null;
 
@@ -47,5 +49,53 @@ extends Mage_Catalog_Block_Product_Abstract
         $price = Mage::helper('tax')
             ->getPrice($this->getProduct(), $this->getProduct()->getFinalPrice(), true);
         return number_format($price, 2, '.', '');
+    }
+    
+    /**
+     * get Rating Value
+     */
+    public function getRatingValue()
+    {
+        $this->getRatingData();
+        return $this->_rating_value;
+    }
+    
+    /**
+     * get Rating Count
+     */
+    public function getRatingCount()
+    {
+        $this->getRatingData();
+        return $this->_rating_count;
+    }
+    
+    public function getRatingData()
+    {
+        if ($this->_rating_value!=-1 || $this->_rating_count!=-1) return true;
+        
+        $this->_rating_value = false;
+        $this->_rating_count = false;
+        
+        try {
+            $summaryData = Mage::getModel('review/review_summary')
+                ->setStoreId(Mage::app()->getStore()->getId())
+                ->load($this->getProduct()->getId());
+
+            if(!$summaryData->getRatingSummary()) return false;
+
+            $rating_value = ($summaryData->getRatingSummary() / 100) * 5;
+            $rating_value = number_format($rating_value, 1, '.', '');
+
+            $this->_rating_value = $rating_value;
+            $this->_rating_count = $summaryData->getReviewsCount();
+            
+            return true;
+        } catch (Exception $e) {
+            //
+            //  \,,/(^_^)\,,/
+            //
+        }
+        
+        return false;
     }
 }
